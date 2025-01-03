25935b08a9609e664813a6c2e9dd87c9296dad1a
Abhishek Kumar <127936615+Akumar4318@users.noreply.github.com>
2025-01-03 23:06:48 +0000

    a

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import ReactDOM from 'react-dom'
 const App = () => {
   return (
-    <div id="main"></div>
-  )
+    <div>
+      <h1>Learning React</h1>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
 }
 
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
